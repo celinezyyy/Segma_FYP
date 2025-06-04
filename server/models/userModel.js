46c1a092bdcf42493e7c623140609377ece0c568
celinezyyy <celineong1016@gmail.com>
2025-06-04 18:01:09 +0800

    complete until server authController(video:2.16.07)

---
+++ server/models/userModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    verifyOtp: {type: String, default: ''},
+    verifyOtpExpiredAt: {type: Number, default: 0},
+    isAccountVerified: {type: Boolean, default: false},
+    resetOtp: {type: String, default: ''},
+    resetOtpExpiredAt: {type: Number, default: 0},
+    createdAt: { type: Date, default: Date.now, required: true },
+})
+
+const userModel = mongoose.model.user || mongoose.model('user', userSchema);
+
+export default userModel;
