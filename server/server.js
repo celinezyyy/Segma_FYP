101b1f4ecc32f750cc604025d7c22bedc3fb6e58
celinezyyy <celineong1016@gmail.com>
2025-06-04 20:30:12 +0800

    complete server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 // npm run server
 
@@ -18,5 +19,6 @@ app.use(cors({credentials: true}));
 // API Endpoints
 app.get('/', (req,res)=> res.send("API working."));
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
