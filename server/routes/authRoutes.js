d7c287bfa7779d4dc5f925e662feaa55c0f32947
celinezyyy <celineong1016@gmail.com>
2025-09-01 16:28:38 +0800

    resolve the multiple click issue

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const authRouter = express.Router();
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
-authRouter.post('/verify-account', userAuth, verifyEmail);
+authRouter.post('/verify-account', verifyEmail);
 authRouter.post('/send-reset-otp', sendResetOtp);
 authRouter.post('/reset-password', resetPassword);
 authRouter.get('/is-auth', userAuth, isAuthenticated)
