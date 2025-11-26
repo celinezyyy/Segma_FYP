5a3fda435f121df56a7d683b04efc67fe0d976e8
celinezyyy <celineong1016@gmail.com>
2025-11-27 01:06:14 +0800

    (WIP) - segmentation

--- server/routes/segmentationRoutes.js
+++ server/routes/segmentationRoutes.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import { prepareSegmentationData, downloadMergedCsv, runSimpleSegmentation } from '../controllers/segmentationController.js';
+import { prepareSegmentationData, downloadMergedCsv } from '../controllers/segmentationController.js';
 import userAuth from '../middleware/userAuth.js';
 
 const segmentationRouter = express.Router();
 
-// Merge customer and order data for segmentation
-segmentationRouter.post('/prepare', userAuth, prepareSegmentationData);
-segmentationRouter.post('/download', userAuth, downloadMergedCsv);
-segmentationRouter.post('/attributes-pairs', userAuth, downloadMergedCsv); // placeholder, may be replaced
-
-// Run simple 2-feature segmentation
-segmentationRouter.post('/run', userAuth, runSimpleSegmentation);
+segmentationRouter.post('/prepare', userAuth, prepareSegmentationData); // merge dataset & persist segmentation
+segmentationRouter.post('/download', userAuth, downloadMergedCsv); // download from persisted segmentation by id
+// segmentationRouter.post('/run', userAuth, runSimpleSegmentation);
 
 export default segmentationRouter;
