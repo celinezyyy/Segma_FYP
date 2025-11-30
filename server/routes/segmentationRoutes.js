33dd1773ee39d5e5f79670783710bb89d3b2d0d9
celinezyyy <celineong1016@gmail.com>
2025-11-30 20:13:33 +0800

    (DONE) - update new route

--- server/routes/segmentationRoutes.js
+++ server/routes/segmentationRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { prepareSegmentationData, downloadMergedCsv } from '../controllers/segmentationController.js';
+import { prepareSegmentationData, downloadMergedCsv, getMergedColumns } from '../controllers/segmentationController.js';
 import userAuth from '../middleware/userAuth.js';
 
 const segmentationRouter = express.Router();
 
 segmentationRouter.post('/prepare', userAuth, prepareSegmentationData); // merge dataset & persist segmentation
 segmentationRouter.post('/download', userAuth, downloadMergedCsv); // download from persisted segmentation by id
+segmentationRouter.get('/:segmentationId/columns', userAuth, getMergedColumns); // list merged CSV columns for UI selection
 // segmentationRouter.post('/run', userAuth, runSimpleSegmentation);
 
 export default segmentationRouter;
