3c6109dc8937e448e84178384b4c16dea4f457f1
celinezyyy <celineong1016@gmail.com>
2025-12-06 11:56:12 +0800

    (DONE) - Enchance side bar

--- client/src/components/UserSidebar.jsx
+++ client/src/components/UserSidebar.jsx
@@ -10,31 +10,17 @@ const UserSidebar = () => {
     <div className="flex">
       <Navbar />
 
-      {/* Toggle button */}
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        type="button"
-        className="fixed top-20 left-2 z-50 p-2 rounded-md text-gray-500 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-700"
-      >
-        <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
-        </svg>
-      </button>
-
       {/* Sidebar */}
       <aside
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
         className={`fixed top-16 left-0 z-40 flex flex-col justify-between h-[calc(100vh-4rem)] bg-gray-50 dark:bg-gray-800 transition-all duration-300 ${
           isOpen ? 'w-64' : 'w-16'
         }`}
       >
+
         <div className="px-2 py-4 overflow-y-auto">
-          <ul className="pt-10 space-y-2 font-medium">
+          <ul className="pt-2 space-y-2 font-medium">
             {/* Home */}
             <li>
               <a
@@ -83,39 +69,17 @@ const UserSidebar = () => {
 
             {/* Segmentation with toggle */}
             <li>
-              <button
-                type="button"
-                onClick={() => setSegOpen(!segOpen)}
-                className="flex items-center w-full p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-                title="Segmentation"
+              <a
+                href="/dataset-selection"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+                title="Dataset"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
                 </svg>
 
-                {isOpen && (
-                  <>
-                    <span className="ml-3 flex-1 text-left">Segmentation</span>
-                    <svg
-                      className={`w-3 h-3 ml-auto transition-transform duration-200 ${
-                        segOpen ? 'rotate-180' : ''
-                      }`}
-                      fill="none"
-                      viewBox="0 0 10 6"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1l4 4 4-4" />
-                    </svg>
-                  </>
-                )}
-              </button>
-
-              {isOpen && (
-                <ul className={`${segOpen ? 'block' : 'hidden'} py-2 space-y-2 pl-11`}>
-                  <li><a href="/dataset-selection" className="block p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">Dataset Selection</a></li>
-                  <li><a href="/segmentation-result" className="block p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700">Segmentation Result</a></li>
-                </ul>
-              )}
+                {isOpen && <span className="ml-3">Segmentation</span>}
+              </a>
             </li>
 
             {/* Dashboard */}
